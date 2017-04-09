e0d235cd8a56dfce59f1074384b79c9933d4b92b
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2017-04-09 10:32:46 +0200

    kernel: blink led on start up to be sure we are booting the kernel. Not tested yet

--- src/init/kernel.c
+++ src/init/kernel.c
@@ -4,14 +4,33 @@
 #include <stddef.h>
 #include <types.h>
 #include <common/peripherals.h>
+#include <common/delay.h>
+#include <gpio/gpio.h>
+#include <led/led.h>
 
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
 #endif
+
+static void blink(u8 ntimes, u32 tdelay)
+{
+    int i = 0;
+
+    for (i=0; i<ntimes; i++)
+    {
+        set_led_state(true);
+        delay(tdelay);
+        set_led_state(false);
+        delay(tdelay);
+    }
+}
 
 void kernel_main(u32 r0, u32 r1, u32 atags)
 {
 	(void) r0;
 	(void) r1;
 	(void) atags;
+
+    init_led();
+    blink(3, 0x10000);
 }
