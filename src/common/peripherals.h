0d92eab051b892eb9a11d01ff000d6c45c48d342
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2017-04-09 07:40:23 +0200

    common: add general peropheral base definitions and define own types for kernel

---
+++ src/common/peripherals.h
@@ -0,0 +1,14 @@
+#ifndef __PERIPHERALS_H_
+#define __PERIPHERALS_H_
+
+#include <common/types.h>
+
+// Raspberry pi peripherals base address
+#define PERIPHERALS_BASE    0x20000000
+
+inline volatile u32 * get_register_address(u32 offset)
+{
+    return (volatile u32 *)(PERIPHERALS_BASE + offset);
+}
+
+#endif // __PERIPHERALS_H_
---
+++ src/common/types.h
@@ -0,0 +1,13 @@
+#ifndef __TYPES_H_
+#define __TYPES_H_
+
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned short u16;
+typedef signed short s16; 
+typedef unsigned int u32;
+typedef signed int s32;
+typedef unsigned long long u64;
+typedef signed long long s64;
+
+#endif // __TYPES_H_
