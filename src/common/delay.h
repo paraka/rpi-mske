98d1c004b40b5d8deb2c98180fd79d749d4069f6
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2017-04-22 07:49:01 +0200

    common: fix indentation

--- src/common/delay.h
+++ src/common/delay.h
@@ -5,7 +5,7 @@
 
 static inline void delay(u32 count)
 {
-        asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
+    asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
                  : "=r"(count): [count]"0"(count) : "cc");
 }
 
