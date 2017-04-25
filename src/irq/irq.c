f0290491f9ab7f5d5b643d4879d152e537936785
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2017-04-25 21:03:34 +0200

    irq: real skeleton of irq. irq vector table and register stuff implemented

--- src/irq/irq.c
+++ src/irq/irq.c
@@ -1,6 +1,8 @@
 #include <common/defs.h>
 #include <irq/irq.h>
 
+static mske_interrupt_vector_t mske_irq_vector_table[INTC_IRQ_TOTAL];
+
 /* page 112 */
 enum IRQ_Register
 {
@@ -16,20 +18,79 @@ enum IRQ_Register
     IRQ_DISABLE_BASIC = (IRQ_BASE + 0x224)
 };
 
-void enable_irqs(void)
+/**
+ *  Enables all IRQ's in the CPU's CPSR register.
+ */
+static inline void irq_enable() 
 {
+    asm volatile("mrs     r0,cpsr");      /* Read in the cpsr register */
+    asm volatile("bic     r0,r0,#0x80");  /* Clear bit 8, (0x80) -- Causes IRQs to be enabled. */
+    asm volatile("msr     cpsr_c, r0");   /* Write it back to the CPSR register */
 }
 
-void disable_irqs(void)
+static inline void irq_disable() 
 {
+    asm volatile("mrs     r0,cpsr");      /* Read in the cpsr register */
+    asm volatile("orr     r0,r0,#0x80");  /* Set bit 8, (0x80) -- Causes IRQs to be disabled */
+    asm volatile("msr     cpsr_c, r0");   /* Write it back to the CPSR register */
 }
 
-void enable_irq(enum IRQ_VECTOR vector)
+#define clz(a) \
+({ unsigned long __value, __arg = (a); \
+     asm ("clz\t%0, %1": "=r" (__value): "r" (__arg)); \
+          __value; })
+
+
+/* default irq_handler */
+static void default_irq_handler(enum mske_irq_vector_id nirq, void *param) 
+{
+    UNUSED(nirq);
+    UNUSED(param);
+}
+
+mske_ret_code_t interrupt_controller_init()
+{
+    int i = 0;
+
+    for(i=0; i<INTC_IRQ_TOTAL; i++)
+    {
+        mske_irq_vector_table[i].irq_fn_handler = default_irq_handler;
+        mske_irq_vector_table[i].param = (void *)0;
+    }
+
+    return MSKE_SUCESS;
+}
+
+mske_ret_code_t register_interrupt(enum mske_irq_vector_id nirq, mske_fn_interrupt_handler irq_handler, void *param)
+{
+    irq_disable();
+    mske_irq_vector_table[nirq].irq_fn_handler = irq_handler;
+    mske_irq_vector_table[nirq].param = param;
+    irq_enable();
+
+    return MSKE_SUCESS;
+}
+
+mske_ret_code_t enable_irqs(void)
+{
+    irq_enable();
+    
+    return MSKE_SUCESS;
+}
+
+mske_ret_code_t disable_irqs(void)
+{
+    irq_disable();
+
+    return MSKE_SUCESS;
+}
+
+void enable_irq(enum mske_irq_vector_id vector)
 {
     UNUSED(vector);
 }
 
-void disable_irq(enum IRQ_VECTOR vector)
+void disable_irq(enum mske_irq_vector_id vector)
 {
     UNUSED(vector);
 }
--- src/irq/irq.h
+++ src/irq/irq.h
@@ -2,23 +2,17 @@
 #define _IRQ_H_
 
 #include <common/peripherals.h>
+#include <common/device.h>
 
 #define IRQ_BASE    (PERIPHERALS_BASE + 0xB000)
 
-void enable_irqs(void);
-void disable_irqs(void);
+#define IRQ_BASIC_BASE_ID   64
 
-/* pág 113 */
-enum IRQ_VECTOR
+#define INTC_IRQ_TOTAL      (64 + 8)
+
+/* page 113 */
+enum mske_irq_vector_id
 {
-    IRQ_ARM_TIMER       = 0,
-    IRQ_ARM_MAILBOX     = 1,
-    IRQ_ARM_DOORBELL0   = 2,
-    IRQ_ARM_DOORBELL1   = 3,
-    IRQ_ARM_GPU0_HALTED = 4,
-    IRQ_ARM_GPU1_HALTED = 5,
-    IRQ_ILLEGAL_ACCESS1 = 6,
-    IRQ_ILLEGAL_ACCESS0 = 7,
     IRQ_AUX             = 29,
     IRQ_I2C_SPI_SLV     = 43,    
     IRQ_PWA0            = 45,
@@ -32,9 +26,29 @@ enum IRQ_VECTOR
     IRQ_SPI             = 54,
     IRQ_PCM             = 55,
     IRQ_UART            = 57,
+    IRQ_ARM_TIMER       = (IRQ_BASIC_BASE_ID + 0),  /* 64 */
+    IRQ_ARM_MAILBOX     = (IRQ_BASIC_BASE_ID + 1),  /* 65 */
+    IRQ_ARM_DOORBELL0   = (IRQ_BASIC_BASE_ID + 2),  /* 66 */
+    IRQ_ARM_DOORBELL1   = (IRQ_BASIC_BASE_ID + 3),  /* 67 */
+    IRQ_ARM_GPU0_HALTED = (IRQ_BASIC_BASE_ID + 4),  /* 68 */
+    IRQ_ARM_GPU1_HALTED = (IRQ_BASIC_BASE_ID + 5),  /* 69 */
+    IRQ_ILLEGAL_ACCESS1 = (IRQ_BASIC_BASE_ID + 6),  /* 70 */
+    IRQ_ILLEGAL_ACCESS0 = (IRQ_BASIC_BASE_ID + 7)   /* 71 */
 };
 
-void enable_irq(enum IRQ_VECTOR vector);
-void disable_irq(enum IRQ_VECTOR vector);
+typedef void (*mske_fn_interrupt_handler)(enum mske_irq_vector_id nirq, void *param);
+
+typedef struct
+{
+    mske_fn_interrupt_handler irq_fn_handler; /* function that handler this interrupt */
+    void *param; /* parameters passed to the IRQ handler */
+} mske_interrupt_vector_t;
+
+mske_ret_code_t interrupt_controller_init();
+mske_ret_code_t register_interrupt(enum mske_irq_vector_id nirq, mske_fn_interrupt_handler irq_handler, void *param);
+void enable_irq(enum mske_irq_vector_id vector);
+void disable_irq(enum mske_irq_vector_id vector);
+mske_ret_code_t enable_irqs(void);
+mske_ret_code_t disable_irqs(void);
 
 #endif /* _IRQ_H_ */
