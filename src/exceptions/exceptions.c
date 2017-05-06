889f8af649bef4751099d9bf1441c3ab235fdb69
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2017-05-06 18:33:46 +0200

    exceptions: implement exception handlers

--- src/exceptions/exceptions.c
+++ src/exceptions/exceptions.c
@@ -1,25 +1,53 @@
-#include <exceptions/exceptions.h>
+#include <common/io.h>
+#include <common/peripherals.h>
+#include <arch/arm/processor.h>
 
-void handler_reset(void)
+static const char *exception[] = {
+    "Reset",
+    "Undefined instruction",
+    "Supervisor Call",
+    "Prefetch Abort",
+    "Data Abort",
+    "Hypervisor",
+    "IRQ",
+    "FIQ"
+};
+
+void handler_reset(mske_context_t *ctx, u32 num)
 {
+    printk("%s: Regs @ %p\n", exception[num], ctx);
+    dump_registers(ctx);
 }
 
-void handler_undef(void)
+void handler_undef(mske_context_t *ctx, u32 num)
 {
+    printk("%s: Regs @ %p\n", exception[num], ctx);
+    dump_registers(ctx);
+    printk("%s @ %08X = %08X\n", exception[num], ctx->lr, mmio_read(ctx->lr));
+    /* skip instruction */
+    ctx->lr += 4;
 }
 
-void handler_svc(void)
+void handler_svc(mske_context_t *ctx, u32 num)
 {
+    printk("%s: Regs @ %p\n", exception[num], ctx);
+    dump_registers(ctx);
 }
 
-void handler_prefetch_abort(void)
+void handler_prefetch_abort(mske_context_t *ctx, u32 num)
 {
+    printk("%s: Regs @ %p\n", exception[num], ctx);
+    dump_registers(ctx);
 }
 
-void handler_data_abort(void)
+void handler_data_abort(mske_context_t *ctx, u32 num)
 {
+    printk("%s: Regs @ %p\n", exception[num], ctx);
+    dump_registers(ctx);
 }
 
-void handler_hypervisor_trap(void)
+void handler_hypervisor_trap(mske_context_t *ctx, u32 num)
 {
+    printk("%s: Regs @ %p\n", exception[num], ctx);
+    dump_registers(ctx);
 }
--- src/exceptions/exceptions.h
+++
@@ -1,6 +0,0 @@
-#ifndef _EXCEPTIONS_H_
-#define _EXCEPTIONS_H_
-
-#include <types.h>
-
-#endif /* _EXCEPTIONS_H_ */
