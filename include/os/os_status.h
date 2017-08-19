1dff058bec0f22907877a98d063f8798de89dddd
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2017-08-19 09:44:57 +0200

    rpi-mske: os: new return status enumeration

---
+++ include/os/os_status.h
@@ -0,0 +1,11 @@
+#ifndef OS_STATUS_H_
+#define OS_STATUS_H_
+
+typedef enum
+{
+    MSKE_OS_OK      = 0,
+    MSKE_OS_TIMEOUT = 1,
+    MSKE_OS_INVALID = 2,
+} mske_return_code_t;
+
+#endif /* OS_STATUS_H_ */
